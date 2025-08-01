0cab9410f321b8d37abe1c7c29a23e3eaa3a0ba1
Isuru Wijayawardhana <isuruanuranga@gmail.com>
2025-08-01 11:53:27 +0530

    Link Tag in react routerdom

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import ProductCard from './components/productCard'
 import HomePage from './pages/homePage.jsx'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import AdminPage from './pages/adminPage.jsx'
-
+import TextPage from './pages/testPage.jsx'
 function App() {
 
   return (
@@ -19,6 +19,7 @@ function App() {
             <Route path='/login'element={<LoginPage/>}/>
             <Route path='/register'element={<RegisterPage/>}/>
             <Route path='/admin/*' element={<AdminPage/>}/>
+            <Route path='/testPage' element={<TextPage/>}/>
           </Routes>
         </div>
       </div>
--- src/pages/adminPage.jsx
+++ src/pages/adminPage.jsx
@@ -1,11 +1,17 @@
+import { Link, Route, Routes } from "react-router-dom";
+
 export default function AdminPage(){
     return(
         <div className="w-full h-screen flex bg-red-500">
             <div className="w-[300px] h-full bg-white">
-
+                <Link to="/admin/products">Product</Link>
             </div>
             <div className="w-[calc(100%-300px)] bg-blue-900 h-full">
-
+                <Routes path="/*">
+                    <Route path="/" element={<h1>Dashboard</h1>}/>
+                    <Route path="/products" element={<h1>Product</h1>}/>
+                    <Route path="/order" element={<h1>Orders</h1>}/>
+                </Routes>
             </div>
         </div>
     )
