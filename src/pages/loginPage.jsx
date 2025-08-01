c31f23425c77c448ef21d834dc5cb16fbedd3b57
Isuru Wijayawardhana <isuruanuranga@gmail.com>
2025-08-01 19:12:16 +0530

    onChange funtion and useSatate Hook

--- src/pages/loginPage.jsx
+++ src/pages/loginPage.jsx
@@ -1,17 +1,29 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function LoginPage(){
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
     return(
         <div className="w-full h-screen bg-[url(./LoginBG.jpg)] bg-center bg-cover flex justify-center items-center">
             <div className="w-[500px] h-[500px] backdrop-blur-sm shadow-2xl rounded-[30px] gap-[20px] relative text-white flex flex-col items-center justify-center">
                 <h1 className="absolute top-[20px] text-2xl font-bold text-center my-5">Login</h1>
                 <div className="w-[350px] flex flex-col">
                     <span className="text-lg ">Email</span>
-                    <input type="text" className="w-[350px] h-[40px] border border-white rounded-xl"/>
+                    <input onChange={
+                        (e)=>{
+                            setEmail(e.target.value)
+                        }
+                    } type="text" className="w-[350px] h-[40px] border border-white rounded-xl"/>
                 </div>
                 <div className="w-[350px] flex flex-col">
                     <span className="text-lg ">Password</span>
-                    <input type="password" className="w-[350px] h-[40px] border border-white rounded-xl"/>
+                    <input onChange={
+                        (e)=>{
+                            setPassword(e.target.value)
+                        }
+                    }
+                    type="password" className="w-[350px] h-[40px] border border-white rounded-xl"/>
                 </div>
                 <button className="w-[350px] h-[40px] bg-blue-500 rounded-xl text-white text-lg mt-5 hover:bg-blue-600 transition-all duration-300">
                     Login
