8c9abc921017c2eee2c9678a5c570e334e792b94
Isuru Wijayawardhana <isuruanuranga@gmail.com>
2025-08-20 15:51:33 +0530

    Day 17 Create product Card

--- src/pages/clientPage.jsx
+++ src/pages/clientPage.jsx
@@ -1,14 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 import Header from "../components/header";
+import ProductPage from "./productPage";
 
 export default function ClientWebPage(){
     return(
         <div className="w-full h-screen max-h-screen ">
             <Header/>
-            <div className="w-full h-[calc(100%-100px)] bg-amber-200">
+            <div className="w-full h-[calc(100%-100px)]">
                 <Routes path="/">
                     <Route path="/" element={<h1 className="text-3xl text-center">Welcome to Home page</h1>}/>
-                    <Route path="/products" element={<h1 className="text-3xl text-center">Product</h1>}/>
+                    <Route path="/products" element={<ProductPage/>}/>
                     <Route path="/review" element={<h1 className="text-3xl text-center">Review</h1>}/>
                     <Route path="/about" element={<h1 className="text-3xl text-center">About us</h1>}/>
                     <Route path="/contact" element={<h1 className="text-3xl text-center">contact us</h1>}/>
