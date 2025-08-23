2d349487ac0b772cc8c20cc103f57f1401d5d8ba
Isuru Wijayawardhana <isuruanuranga@gmail.com>
2025-08-23 15:53:31 +0530

    Day 18 Add product details for product overview page

--- src/pages/productOverView.jsx
+++ src/pages/productOverView.jsx
@@ -35,8 +35,27 @@ export default function ProductOverViewPage(){
                     <div className="w-[49%] h-full flex flex-col justify-center items-center">
                         <ImageSlider images={product.images}/>
                     </div>
-                    <div className="w-[49%] h-full">
-
+                    <div className="w-[49%] h-full flex flex-col items-center pt-[50px]">
+                        <h1 className="text-2xl font-bold">{product.name} <span className="font-light">{product.altName.join("|")}</span></h1>
+                        <p className="text-lg mt-[20px]">{product.description}</p>
+                        <div className="w-full flex flex-col items-center mt-[20px]">
+                            {
+                                product.labelledPrice > product.price ? 
+                                (<p>
+                                    <span className="line-through mr-[10px] text-3xl font-semibold ">{product.labelledPrice.toLocaleString('en-US',{ minimumFractionDigits : 2, maximumFractionDigits: 2})}</span>
+                                    <span className="text-3xl font-bold">{product.price.toLocaleString('en-US',{ minimumFractionDigits : 2, maximumFractionDigits: 2})}</span>
+                                </p>) : 
+                                (<span className="text-3xl font-bold">{product.price.toLocaleString('en-US',{ minimumFractionDigits : 2, maximumFractionDigits: 2})}</span>)
+                            }
+                        </div>
+                        <div className="w-full flex flex-row justify-center items-center mt-[20px] gap-[15px]">
+                            <button className="w-[200px] h-[40px] cursor-pointer rounded-xl shadow-2xl text-white bg-blue-900 border-[3px] border-blue-900 hover:bg-white hover:w-[205px] hover:text-blue-900">
+                                Buy Now
+                            </button>
+                            <button className="w-[200px] h-[40px] cursor-pointer rounded-xl shadow-2xl text-white bg-blue-600 border-[3px] border-blue-600 hover:bg-white hover:w-[205px] hover:text-blue-600">
+                                Add to Cart
+                            </button>
+                        </div>    
                     </div>
                 </div>
             }
