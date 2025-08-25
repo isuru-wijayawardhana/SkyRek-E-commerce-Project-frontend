eb5af59b9d9d27a85b3af7be59f319f2ea62b6dc
Isuru Wijayawardhana <isuruanuranga@gmail.com>
2025-08-25 13:31:14 +0530

    Day 19 create GetCart and AddToCart funtcions

---
+++ utils/cart.jsx
@@ -0,0 +1,43 @@
+export function getCart(){
+    let cartInString = localStorage.getItem("cart")
+
+    if(cartInString == null){
+        cartInString = []
+        localStorage.setItem("cart",cartInString)
+    }
+
+    const cart = JSON.parse(cartInString)
+    return cart
+}
+
+export function addToCart(product,qty){
+    const cart = getCart()
+
+    const existingProductIndex = cart.findIndex((item)=>{ //index eka hoyala denawa
+        return item.productId === product.productId
+    })
+
+    if(existingProductIndex == -1){
+        cart.push(
+            {
+                productId: product.productId,
+                quantity: qty,
+                price: product.price,
+                name: product.name,
+                altName: product.altName,
+                image: product.image
+            }
+        )
+    }else{
+        const newQty = cart[existingProductIndex].quantity + qty
+        if(newQty <= 0){
+            const newCart = cart.filter((item,index)=>{ //Array ekk hadala denawa
+                return index !== existingProductIndex
+            })
+            localStorage.setItem("cart",JSON.stringify(newCart))
+        }else{
+            cart[existingProductIndex].quantity = newQty
+            localStorage.setItem("cart",JSON.stringify(cart))
+        }
+    }
+}
