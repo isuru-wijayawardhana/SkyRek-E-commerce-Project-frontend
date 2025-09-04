cf2393bbde361d191dc6d0733c4739bbcf0677be
Isuru Wijayawardhana <isuruanuranga@gmail.com>
2025-09-04 12:36:26 +0530

    Day 21 mobile Responsive for cart page

--- src/pages/cart.jsx
+++ src/pages/cart.jsx
@@ -7,19 +7,24 @@ export default function CartPage(){
     const [cart , setCart] = useState(getCart())
     const navigate = useNavigate()
     return(
-        <div className="w-full h-screen flex flex-col py-[40px] items-center">
+        <div className="max-w-[100vw] h-screen flex flex-col py-[40px] px-[10px] items-center">
             {
                 cart.map(
                     (item)=>{
                         return(
-                            <div key={item.productId} className="w-[800px] h-[100px] m-[10px] shadow-2xl flex flex-row items-center relative">
-                                <img src={item.image} className="w-[100px] h-[100px] object-cover"/>
-                                <div className="w-[320px] h-full flex flex-col justify-center pl-[10px] ">
-                                    <span className="font-bold">{item.name}</span>
-                                    
-                                    <span className="font-semibold">{item.price.toLocaleString('en-US',{ minimumFractionDigits : 2, maximumFractionDigits: 2})}</span>
+                            <div key={item.productId} className=" w-full md:w-[800px] md:h-[100px] h-[250px] m-[10px] shadow-2xl flex flex-row items-center relative">
+                                <div className="md:w-[100px] w-[200px] justify-center items-center flex flex-col">
+                                    <img src={item.image} className="w-[100px] h-[100px] object-cover"/>
+                                    <div className=" h-full flex-col justify-center pl-[10px] md:hidden flex">
+                                        <span className="font-bold text-center md:text-left text-2xl md:text-md">{item.name}</span>
+                                        <span className="font-semibold text-center">{item.price.toLocaleString('en-US',{ minimumFractionDigits : 2, maximumFractionDigits: 2})}</span>
+                                    </div>
                                 </div>
-                                <div className="w-[190px] h-full flex flex-row justify-center items-center ">
+                                <div className="w-[320px] h-full flex-col justify-center pl-[10px] hidden md:flex">
+                                        <span className="font-bold text-center md:text-left">{item.name}</span>
+                                        <span className="font-semibold">{item.price.toLocaleString('en-US',{ minimumFractionDigits : 2, maximumFractionDigits: 2})}</span>
+                                </div>
+                                <div className="w-[190px] h-full text-4xl md:text-md flex flex-row justify-center items-center ">
                                     <button className="w-[30px] flex justify-center items-center rounded-lg text-white bg-accent cursor-pointer hover:bg-blue-400" onClick={
                                         ()=>{
                                             addToCart(item,-1)
@@ -34,10 +39,10 @@ export default function CartPage(){
                                         }
                                     }>+</button>
                                 </div>
-                                <div className="w-[190px] h-full flex justify-end items-center pr-[10px]">
+                                <div className="w-[190px] h-full text-3xl md:text-md flex justify-end items-center pr-[10px]">
                                     <span className="font-semibold">{(item.quantity * item.price).toLocaleString('en-US',{ minimumFractionDigits : 2, maximumFractionDigits: 2})}</span>
                                 </div>
-                                <button className="w-[30px] h-[30px] absolute right-[-40px] cursor-pointer bg-red-700 shadow rounded-full flex justify-center items-center text-white border-[2px] border-red-700 hover:bg-white hover:text-red-700" onClick={
+                                <button className="w-[30px] h-[30px] absolute top-[0px] right-[0px] md:top-[35px] md:right-[-40px] cursor-pointer bg-red-700 shadow rounded-full flex justify-center items-center text-white border-[2px] border-red-700 hover:bg-white hover:text-red-700" onClick={
                                     ()=>{
                                         addToCart(item,-item.quantity)
                                         setCart(getCart())
@@ -49,7 +54,7 @@ export default function CartPage(){
                     }
                 )
             }
-            <div className="w-[800px] h-[100px] m-[10px] p-[10px] shadow-2xl flex flex-row justify-end items-center relative">
+            <div className="md:w-[800px] w-full h-[100px] m-[10px] p-[10px] shadow-2xl flex flex-row justify-end items-center relative">
                 <span className="font-bold text-2xl mr=[20px]">
                     Total: {getTotal().toLocaleString('en-US',{ minimumFractionDigits : 2, maximumFractionDigits: 2})}
                 </span>
